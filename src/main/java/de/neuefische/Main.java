98a72c9d25bd290410b479cbf560df1e88292254
Dominic <andro5@web.de>
2023-07-06 11:00:08 +0200

    add faculty exercise with tests

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -3,10 +3,67 @@ package de.neuefische;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        countTo100WithWhile();
+        countTo100WithFor();
     }
 
     public static int add (int a, int b){
         return a+b;
+    }
+
+    public static boolean checkIfGreaterThan100(int x){
+        return x>100;
+    }
+
+
+
+    /**User gibt Alter ein und nach einer Prüfung werden folgende Werte zurückgegeben
+     * unter 14 = Kind
+     * zwischen 14 und 16 = Minderjährig
+     * zwischen 16 und 18 = Jugendlich
+     * 18+ = Erwachsen
+     */
+
+    public static String checkAge(int age) {
+        if (age < 16 && age > 13){
+            return "Minderjährig";
+        } else if (age < 18 && age > 15) {
+            return "Jugendlich";
+        } else if (age >= 18) {
+            return "Erwachsen";
+        }
+        return "Kind";
+    }
+
+    public static void countTo100WithWhile(){
+        int count = 0;
+        while (count < 100){
+            count++;
+            System.out.println(count);
+        }
+    }
+
+    public static void countTo100WithFor(){
+        for (int i = 1;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+
+
+//    public static int getFaculty(int number) {
+//        int faculty = 1; //!0
+//        for (int i = 1;i<=number; i++){
+//            faculty*=i;
+//        }
+//        return faculty;
+//    }
+
+    public static int getFaculty(int number) {
+        int faculty = 1; //!0
+        while (number>1){
+            faculty*=number;
+            number--;
+        }
+        return faculty;
     }
 }
--- src/test/java/de/neuefische/MainTest.java
+++ src/test/java/de/neuefische/MainTest.java
@@ -22,5 +22,111 @@ class MainTest {
         assertEquals(solution,actual);
     }
 
+    /**User gibt Alter ein und nach einer Prüfung werden folgende Werte zurückgegeben
+     * unter 14 = Kind
+     * zwischen 14 und 16 = Minderjährig
+     * zwischen 16 und 18 = Jugendlich
+     * 18+ = Erwachsen
+     */
+
+    @Test
+    void whenCheckAge_with10_thenExpectKind(){
+        assertEquals("Kind", Main.checkAge(10));
+    }
+
+    @Test
+    void whenCheckAge_with14_thenExpectMinderjährig(){
+        //GIVEN
+        int age = 14;
+        //WHEN
+        String actual = Main.checkAge(age);
+        //THEN
+        assertEquals("Minderjährig", actual);
+    }
+
+    @Test
+    void whenCheckAge_with16_thenExpectJugendlich(){
+        //GIVEN
+        int age = 16;
+        //WHEN
+        String actual = Main.checkAge(age);
+        //THEN
+        assertEquals("Jugendlich", actual);
+    }
+
+    @Test
+    void whenCheckAge_with18_thenExpectErwachsen(){
+        //GIVEN
+        int age = 18;
+        //WHEN
+        String actual = Main.checkAge(age);
+        //THEN
+        assertEquals("Erwachsen", actual);
+    }
+
+    @Test
+    void whenCheckAge_withMinus1_thenExpectErwachsen(){
+        //GIVEN
+        int age = -1;
+        //WHEN
+        String actual = Main.checkAge(age);
+        //THEN
+        assertEquals("Kind", actual);
+    }
+
+    @Test
+    void whenGetFaculty_with3_thenExpect6(){
+        //GIVEN
+        int number = 3;
+        int expected = 6;
+        //WHEN
+        int actual = Main.getFaculty(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void whenGetFaculty_with5_thenExpect120(){
+        //GIVEN
+        int number = 5;
+        int expected = 120;
+        //WHEN
+        int actual = Main.getFaculty(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void whenGetFaculty_with10_thenExpect3628800(){
+        //GIVEN
+        int number = 10;
+        int expected = 3628800;
+        //WHEN
+        int actual = Main.getFaculty(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void whenGetFaculty_with0_thenExpect1(){
+        //GIVEN
+        int number = 0;
+        int expected = 1;
+        //WHEN
+        int actual = Main.getFaculty(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void whenGetFaculty_withMinus5_thenExpect1(){
+        //GIVEN
+        int number = -5;
+        int expected = 1;
+        //WHEN
+        int actual = Main.getFaculty(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
 
 }
